1e0a8b1d06f66ae2d621fd868721b8d7b14e4ac8
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-11-12 04:28:32 +0530

    ⬆update

--- assets/common.js
+++ assets/common.js
@@ -1,4 +1,4 @@
-const apiKey = process.env.API_KEY;
+const API_KEY = "bY6Q-_0I7DhfBJo9-h7b0TwzKCDQKBAF1h_Q_w-rjAk";
 function convert_to_json(data) {
   return data.json();
 }
--- scripts/my_script.js
+++
@@ -1,7 +0,0 @@
-const common = require('../assets/common');  
-const apiKey = process.env.API_KEY; 
-if (!apiKey) { 
-    throw new Error("API Key is not defined. Please check your GitHub Actions secrets."); 
-} 
-console.log(`API Key: ${apiKey}`); 
-console.log(`Common: ${common}`);
